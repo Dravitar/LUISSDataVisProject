4666be68556cf28f974c363763c793e712be6fe6
Alex Henderson <bohroco@gmail.com>
2023-05-02 10:17:47 +0200

    Create webCode.js

---
+++ webCode.js
@@ -0,0 +1,17 @@
+function showSection(sectionId) {
+	// Hide all sections
+	var sections = document.getElementsByTagName("section");
+	for (var i = 0; i < sections.length; i++) {
+		sections[i].style.display = "none";
+	}
+	// Show the selected section
+	var section = document.getElementById(sectionId + "_section");
+	section.style.display = "block";
+	// Set the active tab button
+	var tabs = document.getElementsByClassName("tab");
+	for (var i = 0; i < tabs.length; i++) {
+		tabs[i].classList.remove("active");
+	}
+	var activeTab = document.querySelector(".tab[data-section='" + sectionId + "']");
+	activeTab.classList.add("active");
+}
