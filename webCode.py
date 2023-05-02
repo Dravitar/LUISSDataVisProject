a06e0e93c7de5fa38eaf1b9ca4501a075a81aeb0
Alex Henderson <bohroco@gmail.com>
2023-05-02 09:01:38 +0200

    Update webCode.py

--- webCode.py
+++ webCode.py
@@ -2,7 +2,7 @@ import pandas as pd
 from pyodide.http import open_url
 
 url = (
-    "https://raw.githubusercontent.com/Dravitar/LUISSDataVisProject/main/zdf_with_new_england.csv?token=GHSAT0AAAAAACA6E665A36J7KOFGTD4C7LKZCKSBFQ"  
+    "https://raw.githubusercontent.com/Dravitar/LUISSDataVisProject/main/zdf_with_new_england.csv"  
     )
 zip_code_data = pd.read_csv(open_url(url))
 
