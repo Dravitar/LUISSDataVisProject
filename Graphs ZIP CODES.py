98d685c7f628cbdf91aa9273aa1cad64d78be5c5
Arianna-Bucca <129767668+Arianna-Bucca@users.noreply.github.com>
2023-05-04 20:36:20 +0200

    Updated with plotly
    still have to fix the box plot

--- Graphs ZIP CODES.py
+++ Graphs ZIP CODES.py
@@ -1,11 +1,13 @@
+import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
 import seaborn as sns
-import matplotlib.patches as mpatches
-from matplotlib.patches import Patch
+import plotly.graph_objs as go
+import plotly.io as pio
 from pyodide.http import open_url
 
 # Load data
+#zdf = pd.read_csv('zdf.csv')
 zdf = pd.read_csv(open_url("https://raw.githubusercontent.com/Dravitar/LUISSDataVisProject/main/zdf.csv"))
 zdf = zdf.rename(columns={'total_rate': 'Total Rate',
                           'state_rate': 'State Rate',
@@ -17,53 +19,103 @@ zdf = zdf.rename(columns={'total_rate': 'Total Rate',
 states = zdf.groupby('Official State Name')['Total Rate', 'State Rate', 'County Rate', 'City Rate', 'Additional Rate'].mean()
 cities = zdf.groupby(['Official State Name', 'Official USPS city name'])['Total Rate', 'State Rate', 'County Rate', 'City Rate', 'Additional Rate'].mean()
 
+# creating the colors dictionary
+colors = {'State Rate':'#A3333D', 'County Rate':'#FFFFFF', 'City Rate':'#1C1F33', 'Additional Rate':'#477998'}
+
+#==================================================================================
+
 # GRAPH 1: Stacked bar charts for highest and lowest total taxes
 ## States with highest total taxes
 data = states.sort_values('Total Rate', ascending=False).head(5).drop(columns='Total Rate')
-data.plot(kind='bar', stacked=True, color=['red', 'ghostwhite', 'darkblue', 'cornflowerblue'], edgecolor='black')
-plt.xlabel('State')
-plt.ylabel('Total Sales Tax Rates')
-plt.ylim([0, 0.12])
-plt.title('Highest Total Sales Tax Rates')
-plt.legend(loc='center left', bbox_to_anchor=(1.0, 0.5))
+# Create traces for each category and a layout for the graph
+traces = []
+for col in data.columns:
+    trace = go.Bar(x=data.index, y=data[col], name=col, marker={'color': colors[col],'line': {'color': 'black', 'width': 2}})
+    traces.append(trace)
+
+layout = go.Layout(title='Highest Total Sales Tax Rates', barmode='stack', xaxis={'title': 'State', 'title_font': {'size': 20}}, yaxis={'title': 'Sales Tax Rate', 'title_font': {'size': 20}}, 
+                   colorway=list(colors.values()), 
+                   legend={'bordercolor': 'black', 'borderwidth': 1, 'font': {'size': 16}}, 
+                   font={'size': 20}, plot_bgcolor='white', paper_bgcolor='white', 
+                   margin={'l': 40, 'b': 40, 't': 80, 'r': 10}, 
+                   bargap=0.15, bargroupgap=0.1)
+# Create and display figure
+fig = go.Figure(data=traces, layout=layout)
+#fig.show()
+# Save the picture in HD
+#pio.write_image(fig, file='Highest Total Sales Tax Rates.png', format='png', width=1200, height=800)
 
 ## States with lowest total taxes (excluding states with no sales taxes)
 data = states.sort_values('Total Rate', ascending=True).head(10).drop(columns='Total Rate')
 data = data.iloc[5:, :]
-data.plot(kind='bar', stacked=True, color=['red', 'ghostwhite', 'darkblue', 'cornflowerblue'], edgecolor='black')
-plt.xlabel('State')
-plt.ylabel('Total Sales Tax Rates')
-plt.ylim([0, 0.12])
-plt.title('Lowest Total Sales Tax Rates')
-plt.legend(loc='center left', bbox_to_anchor=(1.0, 0.5))
+# Create traces for each category and a layout for the graph
+traces = []
+for col in data.columns:
+    trace = go.Bar(x=data.index, y=data[col], name=col, marker={'color': colors[col],'line': {'color': 'black', 'width': 2}})
+    traces.append(trace)
+layout = go.Layout(title='Lowest Total Sales Tax Rates', barmode='stack', xaxis={'title': 'State', 'title_font': {'size': 20}}, yaxis={'title': 'Sales Tax Rate', 'title_font': {'size': 20}}, 
+                   colorway=list(colors.values()), 
+                   legend={'bordercolor': 'black', 'borderwidth': 1, 'font': {'size': 16}}, 
+                   font={'size': 20}, plot_bgcolor='white', paper_bgcolor='white', 
+                   margin={'l': 40, 'b': 40, 't': 80, 'r': 10}, 
+                   bargap=0.15, bargroupgap=0.1)
+# Create and display figure
+fig = go.Figure(data=traces, layout=layout)
+#fig.show()
+# Save the picture in HD
+#pio.write_image(fig, file='Lowest Total Sales Tax Rates.png', format='png', width=1200, height=800)
+
 
 # GRAPH 2: Pie chart for tax percentage by type
 ## Calculate tax percentage for state, county, city, and additional rate
 tax_perc = states[['State Rate', 'County Rate', 'City Rate', 'Additional Rate']].div(states['Total Rate'], axis=0) * 100
-tax_perc = tax_perc.mean().sort_values(ascending=False)
+tax_perc = pd.DataFrame(tax_perc.mean().sort_values(ascending=False))
+# create a list of values for the chart
+values = list(tax_perc[0])
+# create a list of colors for the chart
+colors_list = [colors[idx] for idx in tax_perc.index]
+# create the pie chart
+data=[go.Pie(labels=tax_perc.index, values=values, marker_colors=colors_list,sort=False,
+             direction='clockwise',textposition='outside',hoverinfo='label+percent',marker={'line': {'color': 'black', 'width': 2}})]
+layout=go.Layout(title='Tax Rates by Type',font={'size': 20},paper_bgcolor='white',
+                 legend={'bordercolor': 'black', 'borderwidth': 1, 'font': {'size': 16}, 'xanchor':'right', 'x':1.3})
 
-## Create pie chart
-colors = ["#FE0303", "#F8F8FE", "#00008A", "#6394EC"]
-pal = sns.color_palette(colors)
-fig, ax = plt.subplots()
-wedges, texts, autotexts = ax.pie(tax_perc, colors=pal, autopct='%1.1f%%', pctdistance=1.18, startangle=90, counterclock=False, wedgeprops={'edgecolor': 'black', 'linewidth': 1})
-ax.set_title('Percentage of Taxes by Type')
+fig = go.Figure(data=data, layout=layout)
+# display the chart
+#fig.show()
+# Save the picture in HD
+#pio.write_image(fig, file='Tax Rates by Type.png', format='png', width=1200, height=800)
 
-## Create custom legend
-handles = []
-for i, tax_type in enumerate(tax_perc.index):
-    handles.append(mpatches.Patch(facecolor=pal[i], edgecolor='black', label=tax_type))
-ax.legend(handles=handles, title="Tax Type", loc="center left", bbox_to_anchor=(1.03, 0, 0.5, 1.6))
 
-#plt.show()
+
+# GRAPH 3: Box plot for comparison of tax rates by type
+## Select columns for box plot
+boxplot_data = states[['State Rate', 'County Rate', 'City Rate', 'Additional Rate']]
+
+# Create a box plot
+traces = []
+for col in boxplot_data.columns: 
+    trace = go.Box(y=boxplot_data[col], name=col, marker={'color': colors[col],'line': {'color': 'black', 'width': 2}})
+    traces.append(trace)
+
+# Create the layout
+layout = go.Layout(title='Comparison of Tax Rates by Type', font={'size': 20}, xaxis={'title': 'Tax Rate Types'}, yaxis={'title': 'Rate Value'},
+                   paper_bgcolor='white')
+
+# Create the figure and add the traces and layout
+fig = go.Figure(data=traces, layout=layout)
+
+# Show the plot
+fig.show()
+
 
 # GRAPH 3: Box plot for comparison of tax rates by type
 ## Select columns for box plot
 boxplot_data = states[['State Rate', 'County Rate', 'City Rate', 'Additional Rate']]
 
 ## Create box plot
-colors = ["#FE0303", "#F8F8FE", "#00008A", "#6394EC"]
-pal = sns.color_palette(colors)
+c = ["#A3333D", "#FFFFFF", "#1C1F33", "#477998"]
+pal = sns.color_palette(c)
 fig, ax = plt.subplots()
 bp = ax.boxplot(boxplot_data, patch_artist=True, labels=['State Rate', 'County Rate', 'City Rate', 'Additional Rate'], sym='k.')
 
@@ -83,38 +135,20 @@ ax.set_ylabel('Tax Rate')
 
 #plt.show()
 
-# GRAPH 4: Scatter plot for density vs. total taxes by region
 ## Extract latitude from Geo Point column
 zdf['Latitude'] = zdf['Geo Point'].str.split(',').str[0].astype(float)
-
 # Define the region based on the latitude
 def get_region(latitude):
     if latitude >= 39:
         return 'North'
     else:
         return 'South'
-    
 zdf['Region'] = zdf['Latitude'].apply(get_region)
 
-## Set the colors for the South, Middle, and North regions
-colors = {'South': '#FE0303', 'North': '#00008A'}
+## Set the colors for the South and North region
+cb = {'South': '#A3333D', 'North': '#477998'}
 
-## Create the scatterplot
-fig, ax = plt.subplots()
-for region, group in zdf.groupby('Region'):
-    ax.scatter(group['Density'], group['Total Rate'], c=colors[region], label=region, alpha=0.7)
-
-## Set the title, labels, and legend
-ax.set_title('Density vs Total Taxes by Region')
-ax.set_xlabel('Density')
-ax.set_ylabel('Total Taxes')
-ax.legend()
-
-## Show the plot
-#plt.show()
-
-
-# GRAPH 5: bubble plot
+# GRAPH 4: bubble plot
 bb = zdf.groupby(['Density', 'Total Rate', 'Region']).count()
 bb = pd.DataFrame(bb['Zip Code']).reset_index()
 bb = bb.rename(columns={'Zip Code': 'Number of Cities'})
@@ -122,19 +156,19 @@ bb = bb.rename(columns={'Zip Code': 'Number of Cities'})
 fig, ax = plt.subplots()
 ax.set_title('Density vs Total Taxes by Region')
 sns.scatterplot(bb,x="Density",y="Total Rate",size="Number of Cities",hue='Region',
-                palette=colors,alpha=0.5,sizes=(20, 700))
+                palette=cb,alpha=0.5,sizes=(20, 700))
 
 sb = bb[bb['Region'] == 'South']
 fig, ax = plt.subplots()
 ax.set_title('Density vs Total Taxes by Region - Southern Region')
 sns.scatterplot(sb,x="Density",y="Total Rate",size="Number of Cities",hue='Region',
-                palette=colors,alpha=0.5,sizes=(20, 700))
+                palette=cb,alpha=0.5,sizes=(20, 700))
 
 nb = bb[bb['Region'] == 'North']
 fig, ax = plt.subplots()
 ax.set_title('Density vs Total Taxes by Region - Northern Region')
 sns.scatterplot(nb,x="Density",y="Total Rate",size="Number of Cities",hue='Region',
-                palette=colors,alpha=0.5,sizes=(20, 700))
+                palette=cb,alpha=0.5,sizes=(20, 700))
 
 
 # =============================================================================
@@ -160,20 +194,20 @@ def getZipCodeGraph(zipCode, price):
   # getting the data ready for the pie chart
   tax_perc = data[['State Rate', 'County Rate', 'City Rate', 'Additional Rate']].div(data['Total Rate'], axis=0) * 100
   tax_perc = tax_perc.mean().sort_values(ascending=False)
+  tax_perc = pd.DataFrame(tax_perc)
 
   # drawing the pie
-  colors = ["#FE0303", "#F8F8FE", "#00008A", "#6394EC"]
-  pal = sns.color_palette(colors)
-  fig, ax = plt.subplots()
-  wedges, texts, autotexts = ax.pie(tax_perc, colors=pal, autopct='%1.1f%%', pctdistance=1.18, startangle=90, counterclock=False, wedgeprops={'edgecolor': 'black', 'linewidth': 1})
-  ax.set_title(ttl)
-
-  # creating custom legend
-  handles = []
-  for i, tax_type in enumerate(tax_perc.index):
-      handles.append(mpatches.Patch(facecolor=pal[i], edgecolor='black', label=tax_type))
-  ax.legend(handles=handles, title="Tax Type", loc="center left", bbox_to_anchor=(1.03, 0, 0.5, 1.6))
-  fig.set_tight_layout(True)
+  # create a list of values for the chart
+  values = list(tax_perc[0])
+  # create a list of colors for the chart
+  colors_list = [colors[idx] for idx in tax_perc.index]
+  # create the pie chart
+  data=[go.Pie(labels=tax_perc.index, values=values, marker_colors=colors_list,sort=False,
+               direction='clockwise',textposition='outside',hoverinfo='label+percent',marker={'line': {'color': 'black', 'width': 2}})]
+  layout=go.Layout(title={'text': ttl,'x':0.5},font={'size': 20},paper_bgcolor='white',
+                   legend={'bordercolor': 'black', 'borderwidth': 1, 'font': {'size': 16}, 'xanchor':'right', 'x':1.3})
+    
+  fig = go.Figure(data=data, layout=layout)
 
   elem = Element("image_container")
   elem.write(fig)
